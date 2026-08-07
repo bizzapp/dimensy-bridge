0ba6157bc891c94209779e377669e808d20266f6
rankoms <rankom202@gmail.com>
2026-08-07 10:38:15 +0700

    fix: correct handler for webhook-notification-certificate route

--- routes/routes.go
+++ routes/routes.go
@@ -34,7 +34,7 @@ func SetupRoutes(deps *config.AppDependencies) *gin.Engine {
 	})
 
 	r.POST("/api/v1/webhook-notification-document", deps.WebhookHdl.HandlePSRENotification)
-	r.POST("/api/v1/webhook-notification-certificate", deps.WebhookHdl.HandlePSRENotification)
+	r.POST("/api/v1/webhook-notification-certificate", deps.WebhookHdl.HandleCertificateNotification)
 	// =======================
 
 	clientOriginsRaw := strings.Split(os.Getenv("CLIENT_ORIGIN"), ",")
